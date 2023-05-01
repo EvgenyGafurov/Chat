5a65e2616491ef3ad1e20f7c9b1ed3a8e155a400
EvgenyGafurov <evgenijgafurovv@gmail.com>
2023-05-01 19:08:26 +0500

    добавлено регистрация и вход пользователя

---
+++ script.js
@@ -0,0 +1,18 @@
+let login = document.querySelector('.login-box')
+let registration = document.querySelector('.registration-box')
+
+
+
+
+
+
+
+function Fun (){
+    login.style.display = 'none'
+    registration.style.display='grid'
+   
+}
+
+
+
+
